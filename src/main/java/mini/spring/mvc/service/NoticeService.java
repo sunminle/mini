608d589c45daa3f068fe3c13bb74b18f40ba58dc
rim <rm1113@naver.com>
2023-12-04 16:43:20 +0900

    notice view, list 출력

---
+++ src/main/java/mini/spring/mvc/service/NoticeService.java
@@ -0,0 +1,11 @@
+package mini.spring.mvc.service;
+
+import java.util.List;
+
+import mini.spring.mvc.bean.NoticeDTO;
+
+public interface NoticeService {
+	
+	public List<NoticeDTO> getNoticeList();
+
+}
